0233d56c28654a6685fb810abb30a0f9876d5273
mostfa-frg <mostafamoghawry22@gmail.com>
2026-08-23 22:27:10 +0300

    dump the model

--- Model.py
+++ Model.py
@@ -28,3 +28,15 @@ TARGET = "hvac_load_kw"
 
 X = df[FEATURES]
 y = df[TARGET]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, shuffle=False
+)
+
+model = XGBRegressor(
+    n_estimators=150,
+    learning_rate=0.05,
+    max_depth=6,
+    random_state=42
+)
+model.fit(X_train, y_train)
